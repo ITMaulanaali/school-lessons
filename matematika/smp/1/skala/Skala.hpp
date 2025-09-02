592cb8b1ad1a8de9a30c44a72e23b91f7112f7d2
Lana <it.maulanaali@gmail.com>
2025-09-02 17:22:36 +0700

    update done

--- matematika/smp/1/skala/Skala.hpp
+++ matematika/smp/1/skala/Skala.hpp
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-
-
-class Skala{
-    public:
-        long skala(int jarakPeta, long jarakSebenarnya){
-            return jarakSebenarnya / jarakPeta;
-        }
-
-        long jarakSebenarnya(int jarakPeta, long skala){
-            return jarakPeta * skala;
-        }
-
-        int jarakPeta(long skala, long jarakSebenarnya){
-            return jarakSebenarnya / skala;
-        }
-};
-=======
 #ifndef SKALA_HPP
 #define SKALA_HPP
 
@@ -27,4 +9,3 @@ class Skala{
 };
 
 #endif
->>>>>>> 0fc7294 (Skala-done)
